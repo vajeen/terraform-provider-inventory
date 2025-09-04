60326442e8571b0827789c59661c84d2b1c892b6
Vajeen Karunathilaka <vajeen.karunathilaka@oneflow.com>
2025-09-04 15:15:29 +0200

    yes

--- main.go
+++ main.go
@@ -28,9 +28,6 @@ func main() {
 	flag.Parse()
 
 	opts := providerserver.ServeOpts{
-		// TODO: Update this string with the published name of your provider.
-		// Also update the tfplugindocs generate command to either remove the
-		// -provider-name flag or set its value to the updated provider name.
 		Address: "registry.terraform.io/vajeen/terraform-provider-inventory",
 		Debug:   debug,
 	}
